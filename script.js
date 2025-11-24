c41b2740333cb1b559b38280b6305d0cf284b3a5
Rachman2929 <rachmanhakim502@gmail.com>
2025-11-24 08:11:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contoh sederhana: Menampilkan pesan di konsol saat halaman dimuat
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Website Portofolio Berhasil Dimuat!');
+    
+    // Tempat untuk kode interaktif lainnya
+});
